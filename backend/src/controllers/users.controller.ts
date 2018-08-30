2d79a4bc6b7269c2b3c9d014d50cf7deea799f32
Pablo A. Herrera <pablo.a.herrera@gmail.com>
2018-08-30 16:41:01 -0300

    Added service

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,11 +1,14 @@
 import {Request, Response} from 'express';
-import {getMongoManager, getManager} from 'typeorm';
 import {UserEntity} from '../entities/user';
 import * as bcrypt from 'bcrypt';
+import UsersService from '../services/users.service';
+
+const userService = new UsersService();
 
 export default class UsersController {
+
   getUsers(req: Request, res: Response) {
-    getMongoManager().getRepository(UserEntity).find().then((user) => {
+    userService.findAll().then((user) => {
       res.send(user);
     });
   }
@@ -18,7 +21,7 @@ export default class UsersController {
     bcrypt.hash(req.body.password, 10)
       .then((hash) => {
         user.password = hash;
-        getManager().getRepository(UserEntity).save(user).then((response: any) => {
+        userService.createUser(user).then((response: any) => {
           console.log(response);
           res.send(response);
         })
--- backend/src/services/users.service.ts
+++ backend/src/services/users.service.ts
@@ -1,14 +1,18 @@
 import {UserEntity} from '../entities/user';
-import {getMongoManager} from 'typeorm';
+import {getManager, getMongoManager} from 'typeorm';
+import * as Promise from 'promise';
 
 export default class UsersService {
 
   findAll(): Promise<Array<UserEntity>> {
-    //return this.repository.find();
     return getMongoManager().getRepository(UserEntity).find();
   }
 
-  /*findBy(params: any): Promise<Array<UserEntity>>  {
-    //return this.repository.find(params);
-  }*/
+  findBy(params: any): Promise<Array<UserEntity>> {
+    return getMongoManager().getRepository(UserEntity).find(params);
+  }
+
+  createUser(user: UserEntity): Promise<UserEntity> {
+    return getManager().getRepository(UserEntity).save(user);
+  }
 }
