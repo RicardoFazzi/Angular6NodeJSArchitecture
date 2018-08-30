b76ceb79f89922bf5e5bd2bbc6e78e717302d162
Ricardo Fazzi <ricardofazzi007@gmail.com>
2018-08-30 16:12:16 -0300

    Added login

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,16 +1,26 @@
 import {Request, Response} from 'express';
 import * as jwt from 'jsonwebtoken'
+import * as bcrypt from 'bcrypt';
+import {UserEntity} from '../entities/user';
+import {getMongoManager} from 'typeorm';
 
 export default class AuthController {
 
   async login(req: Request, res: Response) {
-    const body = req.body;
 
-    if (body.username !== 'pablo') {
-      return res.sendStatus(401);
+    let user = await getMongoManager().getRepository(UserEntity).findOne({email: req.body.username});
+
+    if(!user) {
+      res.sendStatus(401);
     }
 
-    let token = jwt.sign({userID: 1}, 'todo-app-super-shared-secret', {expiresIn: '2h'});
-    res.send({token});
+    bcrypt.compare(req.body.password, user.password)
+      .then((auth: boolean) => {
+        if(!auth){
+          res.sendStatus(401);
+        }
+        let token = jwt.sign({userID: 1}, 'todo-app-super-shared-secret', {expiresIn: '2h'});
+        res.send({token});
+      });
   }
 }
